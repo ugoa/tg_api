b446b00db365092aae046cd2b6d459d5e02e7cdb
David.H <hoodavy@gmail.com>
2017-03-03 01:57:02 +0800

    git s

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,15 @@
 class LogsController < ApplicationController
+
+  def index
+    timestamp = Time.at(params[:timestamp].to_i).utc.to_datetime
+
+    logs = ChangeLog
+      .where(object_id: params[:object_id], object_type: params[:object_type])
+      .where('timestamp <= ?', timestamp)
+
+    changes = {}
+    logs.each { |log| changes.merge! log.object_changes }
+
+    render json: changes, status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :csv_importings, only: [:index, :create]
+
+  resources :logs, only: [:index]
 end
