64ef8c8e86fcd0bed179138dbaae5bd612adb58e
David.H <hoodavy@gmail.com>
2017-03-02 23:30:05 +0800

    create action

--- app/controllers/csv_importings_controller.rb
+++ app/controllers/csv_importings_controller.rb
@@ -4,5 +4,24 @@ class CsvImportingsController < ApplicationController
   end
 
   def create
+    require 'csv'
+
+    File.open(params[:file].path, 'r') do |file|
+
+      CSV.foreach(file, headers: true) do |row|
+        # # First line is header, skip
+        # next if index == 0
+        # parsed_row = CSV.parse(line.gsub('\"', '""')).first
+        log = ChangeLog.new(parsed_row.to_h)
+      end
+    end
+
+    # CSV.foreach(text, headers: true) do |line|
+    #   # # First line is header, skip
+    #   # next if index == 0
+    #   # parsed_row = CSV.parse(line.gsub('\"', '""')).first
+    #   byebug
+    #   log = ChangeLog.new(parsed_row.to_h)
+    # end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170302141009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "change_logs", force: :cascade do |t|
+    t.integer  "object_id"
+    t.string   "object_type"
+    t.datetime "timestamp"
+    t.jsonb    "object_changes"
+  end
 
 end
